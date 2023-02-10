a5b2a3ee1bec6e3e3eb81badce462a9087539009
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2023-02-10 16:37:49 +0000

    Fix flagging errors as unsigned

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use std::ffi::c_void;
 use std::mem;
 use windows::core::{GUID, PCWSTR, PWSTR};
 use windows::Win32::Foundation::{HANDLE, HWND};
-use windows::Win32::Security::WinTrust::*;
+use windows::Win32::Security::WinTrust::{WINTRUST_DATA, WINTRUST_DATA_0, WINTRUST_DATA_UICONTEXT,
+                                         WINTRUST_FILE_INFO, WinVerifyTrust, WTD_CHOICE_FILE,
+                                         WTD_REVOKE_NONE, WTD_STATEACTION_CLOSE,
+                                         WTD_STATEACTION_VERIFY, WTD_UI_NONE};
 
 extern crate windows;
 
@@ -39,10 +42,9 @@ unsafe fn walk_path(args: Vec<String>) {
 }
 
 unsafe fn process_file(path: &str) {
-
     let mut file_data = WINTRUST_FILE_INFO {
         hFile: HANDLE::default() as HANDLE,
-        pcwszFilePath: PCWSTR(path.as_ptr() as *mut _),
+        pcwszFilePath: PCWSTR::from_raw(path.encode_utf16().collect::<Vec<u16>>().as_mut_ptr()),
         pgKnownSubject: 0 as *mut GUID,
         cbStruct: (mem::size_of::<&WINTRUST_FILE_INFO> as u32),
     };
@@ -65,12 +67,50 @@ unsafe fn process_file(path: &str) {
     wintrust_data.dwUIContext = WINTRUST_DATA_UICONTEXT::default();
 
     let mut wintrust_action_generic_verify_v2 = GUID::from_values(0xaac56b, 0xcd44, 0x11d0, [0x8c, 0xc2, 0x00, 0xc0, 0x4f, 0xc2, 0x95, 0xee]);
+
     let state_ptr: *mut c_void = &mut wintrust_data as *mut _ as *mut c_void;
     let status = WinVerifyTrust(HWND::default(), &mut wintrust_action_generic_verify_v2, state_ptr);
 
-    match status {
+    const TRUST_E_SYSTEM_ERROR: u32 = 0x80096001;
+    const TRUST_E_NO_SIGNER_CERT: u32 = 0x80096002;
+    const TRUST_E_COUNTER_SIGNER: u32 = 0x80096003;
+    const TRUST_E_CERT_SIGNATURE: u32 = 0x80096004;
+    const TRUST_E_TIME_STAMP: u32 = 0x80096005;
+    const TRUST_E_BAD_DIGEST: u32 = 0x80096010;
+    const TRUST_E_BASIC_CONSTRAINTS: u32 = 0x80096019;
+    const TRUST_E_FINANCIAL_CRITERIA: u32 = 0x8009601E;
+    const TRUST_E_PROVIDER_UNKNOWN: u32 = 0x800B0001;
+    const TRUST_E_ACTION_UNKNOWN: u32 = 0x800B0002;
+    const TRUST_E_SUBJECT_FORM_UNKNOWN: u32 = 0x800B0003;
+    const TRUST_E_SUBJECT_NOT_TRUSTED: u32 = 0x800B0004;
+    const TRUST_E_NOSIGNATURE: u32 = 0x800B0100;
+    const CERT_E_UNTRUSTEDROOT: u32 = 0x800B0109;
+    const TRUST_E_FAIL: u32 = 0x800B010B;
+    const TRUST_E_EXPLICIT_DISTRUST: u32 = 0x800B0111;
+    const CERT_E_CHAINING: u32 = 0x800B010A;
+    const CRYPT_E_FILE_ERROR: u32 = 0x80092003;
+
+    match status as u32{
         0 => (),
-        _ => println!("Unsigned DLL found: {}", &path),
+        TRUST_E_SYSTEM_ERROR => println!("Unsigned DLL found: {} - result TRUST_E_SYSTEM_ERROR", &path),
+        TRUST_E_NO_SIGNER_CERT => println!("Unsigned DLL found: {} - result TRUST_E_NO_SIGNER_CERT", &path),
+        TRUST_E_COUNTER_SIGNER => println!("Unsigned DLL found: {} - result TRUST_E_COUNTER_SIGNER", &path),
+        TRUST_E_CERT_SIGNATURE => println!("Unsigned DLL found: {} - result TRUST_E_CERT_SIGNATURE", &path),
+        TRUST_E_TIME_STAMP => println!("Unsigned DLL found: {} - result TRUST_E_TIME_STAMP", &path),
+        TRUST_E_BAD_DIGEST => println!("Unsigned DLL found: {} - result TRUST_E_BAD_DIGEST", &path),
+        TRUST_E_BASIC_CONSTRAINTS => println!("Unsigned DLL found: {} - result TRUST_E_BASIC_CONSTRAINTS", &path),
+        TRUST_E_FINANCIAL_CRITERIA => println!("Unsigned DLL found: {} - result TRUST_E_FINANCIAL_CRITERIA", &path),
+        TRUST_E_PROVIDER_UNKNOWN => println!("Unsigned DLL found: {} - result TRUST_E_PROVIDER_UNKNOWN", &path),
+        TRUST_E_ACTION_UNKNOWN => println!("Unsigned DLL found: {} - result TRUST_E_ACTION_UNKNOWN", &path),
+        TRUST_E_SUBJECT_FORM_UNKNOWN => println!("Unsigned DLL found: {} - result TRUST_E_SUBJECT_FORM_UNKNOWN", &path),
+        TRUST_E_SUBJECT_NOT_TRUSTED => println!("Unsigned DLL found: {} - result TRUST_E_SUBJECT_NOT_TRUSTED", &path),
+        TRUST_E_NOSIGNATURE => println!("Unsigned DLL found: {} - result TRUST_E_NOSIGNATURE", &path),
+        CERT_E_UNTRUSTEDROOT => println!("Unsigned DLL found: {} - result CERT_E_UNTRUSTEDROOT", &path),
+        TRUST_E_FAIL => println!("Unsigned DLL found: {} - result TRUST_E_FAIL", &path),
+        TRUST_E_EXPLICIT_DISTRUST => println!("Unsigned DLL found: {} - result TRUST_E_EXPLICIT_DISTRUST", &path),
+        CERT_E_CHAINING => println!("Unsigned DLL found: {} - result CERT_E_CHAINING", &path),
+        CRYPT_E_FILE_ERROR => (), //println!("Error: {}: CRYPT_E_FILE_ERROR", &path),
+        _ => println!("Unknown error: {} - code: 0x{:X}", &path, status),
     }
 
     wintrust_data.dwStateAction = WTD_STATEACTION_CLOSE;
